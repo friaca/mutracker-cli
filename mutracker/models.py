e7eb94bf10ee9a1b8fb26560bc397c8cd465c29a
friaca <v.fria@outlook.com>
2021-03-28 23:17:14 -0300

    [del] removing whitespace

--- mutracker/models.py
+++ mutracker/models.py
@@ -5,29 +5,22 @@ from typing import List
 class BaseModel:
   # Database UID
   id: int
-
   # Entry create date timestamp
   dt_create: datetime
 
 class Release(BaseModel):
   # Name of the album
   name: str
-
   # Release date of the record
   dt_release: datetime 
-
   # Artist's/band's name
   artist: str
-
   # Release genres
   genres: List[str]
-
   # Release type (Album or EP)
   type: str
-
   # Wheter the user has listened to the release or not
   status_listened: bool
-
   # If listened, the date in which the user listened to the album
   listened_date: datetime
 

3fd893b382f543fab5da7a7c8e30826767ce64e8
friaca <v.fria@outlook.com>
2021-03-29 00:43:53 -0300

    [add] init database with mocked data for testing

--- mutracker/__init__.py
+++ mutracker/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import pathlib
 import os
-from .db.migrations import run_migrations
+from .db.migrations import run_migrations, insert_mock_data
 from dotenv import dotenv_values
 dotenv_values()
 
@@ -20,7 +20,13 @@ if not pathlib.Path(config['DB_PATH']).is_file():
   try:
     print('Running migrations...')
     run_migrations(config['DB_PATH'])
+
+    if config['ENV'] == 'development' and config['MOCK_DATA'] == 'yes':
+      print('Inserting mocked data on the database...')
+      insert_mock_data(config['DB_PATH'])
+      print('Mocked data insertion complete.')
+
     print('Initial migrations complete, the database is ready.')
   except Exception as e:
-    print("Couldn't open database!", e)
+    print("Couldn't open database!\n[ERROR] ", e)
     sys.exit(1)
--- mutracker/db/migrations.py
+++ mutracker/db/migrations.py
@@ -12,7 +12,7 @@ def run_migrations(db_path: str):
     dt_release TEXT NOT NULL,
     type INTEGER NOT NULL,
     status_listened INTEGER NOT NULL, 
-    listened_date TEXT NOT NULL
+    listened_date TEXT
   )""")
 
   # Release's type table
@@ -22,9 +22,7 @@ def run_migrations(db_path: str):
   )""")
   
   # Release's type rows
-  cur.execute("""INSERT INTO type (name)
-    VALUES ('Album'), ('EP')
-  """)
+  cur.execute("INSERT INTO type (name) VALUES ('Album'), ('EP')")
 
   # Genres table
   cur.execute("""CREATE TABLE IF NOT EXISTS genre (
@@ -32,6 +30,29 @@ def run_migrations(db_path: str):
     name TEXT NOT NULL,
     id_release INTEGER NOT NULL
   )""")
+
+  con.commit()
+  con.close()
+
+def insert_mock_data(db_path: str):
+  con = sqlite3.connect(db_path)
+  cur = con.cursor()
+
+  cur.execute("""INSERT INTO release (name, dt_release, artist, type, status_listened, listened_date) 
+    VALUES ('We Will Always Love You', '11-12-2020', 'The Avalanches', 1, 0, NULL),
+    ('The Piper at the Gates of Dawn', '21-05-1967', 'Pink Floyd', 1, 0, NULL),
+    ('무너지기 (Crumbling)', '31-07-2018', '공중도둑 [Mid-Air Thief]', 1, 1, NULL)
+  """)
+
+  cur.execute("""INSERT INTO genre (name, id_release) 
+    VALUES ('Electronic', 1),
+    ('Neo-Psychedelia', 1),
+    ('Psychedelic Rock', 2),
+    ('Psychedelic Pop', 2),
+    ('Neo-Psychedelia', 3),
+    ('Folktronica', 3),
+    ('Psychedelic Pop', 3)
+  """)
 
   con.commit()
   con.close()

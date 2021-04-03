66b2c9c3769b4a5df787f127c280093ed01031fb
friaca <v.fria@outlook.com>
2021-04-03 02:31:10 -0300

    [del] removing things i don't know the purpose of

--- mutracker/service.py
+++ mutracker/service.py
@@ -7,17 +7,13 @@ class ReleaseService():
     self._repository = ReleaseRepository()
 
   def list_release(self, argv) -> List[Release] or None:
-    results = None
-
     if argv in ['all', 'listened', 'pending']:
-      results = self._repository.list(argv)
+      return self._repository.list(argv)
     elif argv == 'query':
       pass
     else:
       print(f'Option {argv} not valid for --list')
       return
-    
-    return results
 
   def find_release(argv):
     pass

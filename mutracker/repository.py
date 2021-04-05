cbeba3cd06075285422bc4f80ccb4a20d7d3d988
friaca <v.fria@outlook.com>
2021-04-04 21:18:52 -0300

    [add/change] `join_query`, `map_release`, `get_genres`
    - static DbUtils method `join_query` to join SQL statements
    - `map_release` in ReleaseRepository so I don't have to map the SQL results in every method
    -  `get_genres` now returns the genres list instead of a joined string

--- mutracker/repository.py
+++ mutracker/repository.py
@@ -9,11 +9,16 @@ class ReleaseRepository():
     self._database = Database(config['DB_PATH'])
 
   def map_release(self, query_results):
-    return list(map(lambda r: Release(*r), query_results))
+    releases = list(map(lambda r: Release(*r), query_results))
+
+    for release in releases:
+      release.genres = self.get_genres(release.id)
+
+    return releases
 
   def get_genres(self, id):
     sql = f'SELECT name FROM genre WHERE id_release = ?'
-    return ', '.join(map(lambda name_tuple: name_tuple[0], self._database.query(sql, (id,))))
+    return list(map(lambda name_tuple: name_tuple[0], self._database.query(sql, (id,))))
 
   def list(self, which: str):
     sql = ['SELECT * FROM release']
@@ -24,15 +29,11 @@ class ReleaseRepository():
       sql += ['WHERE status_listened = 0']
     
 
-    return map(lambda r: Release(*r), self._database.query(' '.join(sql)))
+    return self.map_release(self._database.query(DbUtils.join_query(sql)))
 
   def find(self, columns: List[str], value: str):
     db_utils = DbUtils.or_like_clause(columns)
-
-    sql = ' '.join(['SELECT * FROM release', 'WHERE', db_utils])
+    sql = DbUtils.join_query(['SELECT * FROM release', 'WHERE', db_utils])
     releases = self.map_release(self._database.query(sql, (f"%{value}%",) * len(columns)))
-    
-    for release in releases:
-      release.genres = self.get_genres(release.id)
     
     return releases
--- mutracker/utils.py
+++ mutracker/utils.py
@@ -2,6 +2,10 @@ from typing import List
 
 class DbUtils():
   @staticmethod
+  def join_query(statements: List[str]):
+    return ' '.join(statements)
+
+  @staticmethod
   def or_like_clause(columns: List[str]):
     mapped = map(lambda column: f"{column} LIKE ?", columns)
     return ' or '.join(mapped)

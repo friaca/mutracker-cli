59ec2eb460b0a42a8554b0c61d57fc89544ef335
friaca <v.fria@outlook.com>
2021-04-05 00:07:36 -0300

    [add] `Utils` class and `is_int` method

--- mutracker/utils.py
+++ mutracker/utils.py
@@ -9,3 +9,11 @@ class DbUtils():
   def or_like_clause(columns: List[str]):
     mapped = map(lambda column: f"{column} LIKE ?", columns)
     return ' or '.join(mapped)
+
+class Utils():
+  @staticmethod
+  def is_int(s):
+    try:
+        return float(str(s)).is_integer()
+    except:
+        return False

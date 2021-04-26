092732e29b48cd1e6074ee62b8e1b93c9cc773e7
friaca <v.fria@outlook.com>
2021-04-25 22:06:48 -0300

    [change] removing exists check from `config_db`

--- mutracker/database.py
+++ mutracker/database.py
@@ -102,17 +102,16 @@ def insert_mock_data(db_path: str):
   db.close()
   
 def config_db(config):
-  if not pathlib.Path(config['DB_PATH']).is_file():
-    try:
-      print('Running migrations...')
-      run_migrations(config['DB_PATH'])
+  try:
+    print('Running migrations...')
+    run_migrations(config['DB_PATH'])
 
-      if config['ENV'] == 'development' and config['MOCK_DATA'] == 'yes':
-        print('Inserting mocked data on the database...')
-        insert_mock_data(config['DB_PATH'])
-        print('Mocked data insertion complete.')
+    if config['ENV'] == 'development' and config['MOCK_DATA'] == 'yes':
+      print('Inserting mocked data on the database...')
+      insert_mock_data(config['DB_PATH'])
+      print('Mocked data insertion complete.')
 
-      print('Initial migrations complete, the database is ready.')
-    except Exception as e:
-      print("Couldn't open database!\n[ERROR] ", e)
-      sys.exit(1)
+    print('Initial migrations complete, the database is ready.')
+  except Exception as e:
+    print("Couldn't open database!\n[ERROR] ", e)
+    sys.exit(1)

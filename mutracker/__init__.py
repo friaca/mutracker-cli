4d90aad4d9f2ee62a6c020994bbb69e4ae005790
friaca <v.fria@outlook.com>
2021-04-25 22:03:58 -0300

    [change] saving db to appdata/home dir

--- mutracker/__init__.py
+++ mutracker/__init__.py
@@ -1,14 +1,25 @@
-from .database import config_db
+from .database import config_db, insert_mock_data, run_migrations
 from dotenv import dotenv_values
 import pathlib
 import os
 
-ROOT = pathlib.Path(__file__).parent.absolute()
-DEV_DB_PATH = os.path.join(os.path.sep, ROOT, 'mutracker.db')
+DB_NAME = 'mutracker.db'
+
+if os.name == 'nt':
+  DB_PATH = os.getenv('APPDATA')
+  FOLDER_NAME = 'mutracker'
+elif os.name == 'posix':
+  DB_PATH = os.getenv('HOME')
+  FOLDER_NAME = '.mutracker'
+
+DB_DIR = os.path.join(DB_PATH, FOLDER_NAME)
+FULL_DB_PATH = os.path.join(DB_DIR, DB_NAME)
 
 config = { 
-  **{ 'DB_PATH': DEV_DB_PATH }, 
+  **{ 'DB_PATH': FULL_DB_PATH }, 
   **dotenv_values('.env') 
 }
 
-config_db(config)
+if not pathlib.Path(DB_DIR).is_dir():
+  pathlib.Path(DB_DIR).mkdir(parents=True)
+  config_db(config)

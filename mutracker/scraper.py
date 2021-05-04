b3950a6d402112f91cf2c495ceeefe667d4ee586
friaca <v.fria@outlook.com>
2021-05-03 23:35:50 -0300

    [add] finishing scraper

--- mutracker/scraper.py
+++ mutracker/scraper.py
@@ -1,9 +1,10 @@
 import random
 import requests
+import re
 from bs4 import BeautifulSoup
 from rich.console import Console
 from os import system
-from .utils import USER_AGENTS
+from .utils import USER_AGENTS, MONTHS
 
 console = Console()
 
@@ -30,23 +31,46 @@ def fetch_release(url: str = None, stream: str = None):
     system.exit(1)
 
   with console.status("[bold green]Processing...") as status:
+    def select_name(dom):
+      full_name = dom.select('.page_section .album_title')[0].get_text(strip=True)
+      by_index = full_name.index('By')
+      return full_name[:by_index]
+
+    def select_dt_release(dom):
+      full_date = dom.findAll('th', {'class':'info_hdr'})[2].find_next().get_text(strip=True)
+      regex = r'^(\d+) ([a-zA-Z]+)(\d+)$'
+      matches = re.search(regex, full_date)
+      return f'{matches.group(3)}-{MONTHS[matches.group(2)]}-{matches.group(1)}'
+    
+    def select_type(dom):
+      value = dom.findAll('th', {'class':'info_hdr'})[1].find_next().get_text(strip=True)
+      return value
+
     selectors = {
-      'name': lambda dom: dom.select('.page_section .album_title')[0],
-      'artist': lambda dom: dom.select('.page_section .artist')[0],
-      'dt_release': lambda dom: dom.findAll('th', {'class':'info_hdr'})[2],
-      'genres': lambda dom: dom.select('.release_pri_genres')[0],
-      'type': lambda dom: dom.findAll('th', {'class':'info_hdr'})[1].find_next()
+      'name': lambda dom: select_name(dom),
+      'artist': lambda dom: dom.select('.page_section .artist')[0].get_text(strip=True),
+      'dt_release': lambda dom: select_dt_release(dom),
+      'genres': lambda dom: dom.select('.release_pri_genres')[0].get_text(strip=True),
+      'type': lambda dom: select_type(dom)
     }
     
     parsed = BeautifulSoup(content, 'html.parser')
-    release_map = {}
+    release_map = {
+      'id': None,
+      'name': None,
+      'artist': None,
+      'genres': None,
+      'dt_release': None,
+      'type': None,
+      'dt_create': None,
+      'status_listened': None,
+      'dt_listened': None,
+      'notes': None,
+      'genres': None
+    }
 
     for key, fn in selectors.items():
-      elem = fn(parsed)
-      print(elem)
-      text = elem.get_text(strip=True)
-      print(text)
-      release_map[key] = text
+      value = fn(parsed)
+      release_map[key] = value
 
-  print(release_map)
-  pass
+  return release_map
